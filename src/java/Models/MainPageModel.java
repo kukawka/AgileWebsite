347a1b42a9d0d772611dbeec0b4c2b456a1efe1e
kkostov5 <k_kostov95@abv.bg>
2017-02-21 17:46:24 +0000

    MainPage
    Servlets and Model Created.

---
+++ src/java/Models/MainPageModel.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+import Util.DBConnection;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Krasi
+ */
+public class MainPageModel {
+    
+public ResultSet getPOS()
+    {
+        
+        ResultSet resultPOS = null;
+        Statement statement = null;
+        try
+        {
+           Connection con;
+           con = DBConnection.createConnection();
+           statement = con.createStatement(); 
+           resultPOS = statement.executeQuery("Select * from programme_of_study");
+           con.close();
+           return resultPOS;
+           
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+       
+        return null;
+    }
+    
+    public ResultSet getModules(int ID)
+    {
+        
+        ResultSet resultModules = null;
+        Statement statement = null;
+        try
+        {
+           Connection con;
+           con = DBConnection.createConnection();
+           statement = con.createStatement(); 
+           resultModules = statement.executeQuery("Select ID,Name from module where POS=" + ID);
+           con.close();
+           return resultModules;
+           
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+       
+        return null;
+    }
+}
---
+++ src/java/Servlets/MainPage.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servlets;
+
+import Models.MainPageModel;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Krasi
+ */
+@WebServlet(name = "MainPage", urlPatterns = {"/MainPage"})
+public class MainPage extends HttpServlet {
+
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        MainPageModel mnm = new MainPageModel();
+        request.setAttribute("POS",mnm.getPOS());
+        
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
