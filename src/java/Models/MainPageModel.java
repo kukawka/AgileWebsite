960ea2bf53181f90dd2f50d54aa766d3c66b0d13
AllBecomesGood <paschnell@dundee.ac.uk>
2017-02-21 21:16:03 +0000

    getQuizzes
    Added getQuizzes method and some comments. 2nd Try since previously it
    didnt add anything.

--- src/java/Models/MainPageModel.java
+++ src/java/Models/MainPageModel.java
@@ -17,6 +17,10 @@ import java.sql.Statement;
  */
 public class MainPageModel {
     
+    /* Upon mainpage loading the Programmes of Study (PoS) will be pulled from the Database and used to populate the 
+     * available buttons in the Nav-bar on the side.
+     * 
+    */ 
 public ResultSet getPOS()
     {
         
@@ -27,7 +31,7 @@ public ResultSet getPOS()
            Connection con;
            con = DBConnection.createConnection();
            statement = con.createStatement(); 
-           resultPOS = statement.executeQuery("Select * from programme_of_study");
+           resultPOS = statement.executeQuery("SELECT * FROM programme_of_study");
            con.close();
            return resultPOS;
            
@@ -40,7 +44,10 @@ public ResultSet getPOS()
         return null;
     }
     
-    public ResultSet getModules(int ID)
+/* Upon selection of a PoS all related modules will be pulled from the DB and used to populate the side Nav-bar further.
+ *
+*/
+public ResultSet getModules(int ID)
     {
         
         ResultSet resultModules = null;
@@ -50,7 +57,7 @@ public ResultSet getPOS()
            Connection con;
            con = DBConnection.createConnection();
            statement = con.createStatement(); 
-           resultModules = statement.executeQuery("Select ID,Name from module where POS=" + ID);
+           resultModules = statement.executeQuery("SELECT ID,Name FROM module WHERE POS=" + ID);
            con.close();
            return resultModules;
            
@@ -62,4 +69,23 @@ public ResultSet getPOS()
        
         return null;
     }
+/* Selecting a module will lead to all quizzes for that module to be pulled from the DB and displayed on the entire page beside the Nav-bar.
+ * @params ID of selected module
+*/
+public ResultSet getQuizzes(int ID)
+{   
+    ResultSet resultQuizzes = null;
+    Statement statement = null;
+    try{
+    Connection con;
+    con = DBConnection.createConnection();
+    resultQuizzes = statement.executeQuery("SELECT ID,Title FROM quiz WHERE moduleID="+ID);
+    con.close();
+    return resultQuizzes;
+    
+    }catch(SQLException e){
+        e.printStackTrace();    
+    }
+    return null;
+}
 }
