a0005e7968b21bd67513b98392960fc0c671d566
kukawka <Dagi@MacBook-Pro-Slawomir.local>
2017-03-06 13:25:36 +0000

    tests for getResults set up

--- test/Models/QuizTest.java
+++ test/Models/QuizTest.java
@@ -7,6 +7,7 @@ package Models;
 
 import Beans.Question;
 import Beans.QuizDetails;
+import Beans.QuizResults;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import junit.framework.TestCase;
@@ -22,28 +23,50 @@ import static org.junit.Assert.*;
  * @author Dagi
  */
 public class QuizTest extends TestCase {
-    private Quiz instance ;
+
+    private Quiz instance;
     private LocalDate creationDate;
-    
+
     public QuizTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
-    public void setUp() {        
+    public void setUp() {
         instance = new Quiz();
-        creationDate=null ;
+        creationDate = null;
     }
-    
+
     @After
     public void tearDown() {
+    }
+
+    /**
+     * Test of getQuizResults method, of class Quiz.
+     */
+    @Test
+    public void testGetQuizResults() {
+        System.out.println("getResults");
+
+        QuizResults result = instance.getQuizResults(1);
+        
+        //there're currently 3 scores for the quiz so first of all I need to check if 3 scores are retrieved
+        assertEquals("The right number of scores wasn't returned", 3, result.getScores().size());
+        assertNotEquals("Not all scores were returned", 2, result.getScores().size());
+        assertNotEquals("Only one score was returned", 1, result.getScores().size());
+        assertNotEquals("Too many scores were returned", 4, result.getScores().size());
+        
+        QuizResults expResult = new QuizResults();
+        //to be continued
+
+        assertNotNull("Database connection failed", result);
     }
 
     /**
@@ -58,44 +81,44 @@ public class QuizTest extends TestCase {
         expResult.setTitle("QuizTest11");
         expResult.setAvailability(true);
         expResult.setDate("2017-02-17");
-        ArrayList <Question> questions=new ArrayList<Question>() ;
-        Question q1=new Question() ;
+        ArrayList<Question> questions = new ArrayList<Question>();
+        Question q1 = new Question();
         //q1.setCorrectAnswerID(0);
         q1.setQuestionText("wddd");
         q1.setExplanation("asfas");
-        questions.add(q1) ;
-        
-        Question q2=new Question() ;
+        questions.add(q1);
+
+        Question q2 = new Question();
         //q2.setCorrectAnswerID(1);
         q2.setQuestionText("AAAA");
         q2.setExplanation("ffff");
-        questions.add(q2) ;
-        
-        Question q3=new Question() ;
+        questions.add(q2);
+
+        Question q3 = new Question();
         //q3.setCorrectAnswerID(-1);
         q3.setQuestionText("ffff");
         q3.setExplanation("www");
-        questions.add(q3) ;
-        
+        questions.add(q3);
+
         expResult.setQuestions(questions);
-       
+
         QuizDetails result = instance.getQuiz(quizID);
 
         assertEquals("The right title wasn't returned", expResult.getTitle(), result.getTitle());
         assertEquals("The right date  wasn't returned", expResult.getDate(), result.getDate());
         assertEquals("The right availability status wasn't returned", expResult.getAvailability(), result.getAvailability());
-       /* assertEquals("The right question text wasn't returned", expResult.getQuestions().get(0).getQuestionText(), result.getQuestions().get(0).getQuestionText());
+        /* assertEquals("The right question text wasn't returned", expResult.getQuestions().get(0).getQuestionText(), result.getQuestions().get(0).getQuestionText());
         assertEquals("The right question text wasn't returned", expResult.getQuestions().get(1).getQuestionText(), result.getQuestions().get(1).getQuestionText());
         assertEquals("The right question text wasn't returned", expResult.getQuestions().get(2).getQuestionText(), result.getQuestions().get(2).getQuestionText());
         assertEquals("The right correct question ID wasn't returned", expResult.getQuestions().get(0).getCorrectAnswers(), result.getQuestions().get(0).getCorrectAnswers());
         assertEquals("The right explanation text wasn't returned", expResult.getQuestions().get(0).getExplanation(), result.getQuestions().get(0).getExplanation());*/
-        String expAnsw[]= new String[4];
-        expAnsw[0]="Example Answer" ;
+        String expAnsw[] = new String[4];
+        expAnsw[0] = "Example Answer";
         /*String answ[]=result.getQuestions().get(0).getAnswers();
         
         assertEquals("The right answer wasn't returned", expAnsw[0], answ[0]);*/
-        
-        assertNotNull("Database connection failed", result) ;
+
+        assertNotNull("Database connection failed", result);
     }
 
     /**
@@ -107,12 +130,11 @@ public class QuizTest extends TestCase {
         String title = "test quiz";
         String moduleID = "1";
         int available = 1;
-        creationDate=LocalDate.now();
-        
-        
+        creationDate = LocalDate.now();
+
         int expResult = -1;
         int result = instance.RegisterQuiz(title, moduleID, available, creationDate);
-        
+
         //result shouldn't return -1, but instead the ID of the quiz
         assertNotSame("An ID was generated for the Quiz.", expResult, result);
         // TODO review the generated test code and remove the default call to fail.
@@ -130,7 +152,7 @@ public class QuizTest extends TestCase {
         int quizID = 5;
         int questionNumber = 1;
         int expResult = -1;
-        
+
         int result = instance.SubmitQuestion(questionText, explanationText, quizID, questionNumber);
         //result shouldn't return -1, but instead the ID of the question
         assertNotSame("An ID was generated for the question.", expResult, result);
@@ -146,14 +168,14 @@ public class QuizTest extends TestCase {
         System.out.println("SubmitAnswer");
         String answerText = "test answer";
         int correct = 0;
-        
+
         int questionNumber = 1;
         int expResult = 1;
-        
+
         int result = instance.SubmitAnswer(answerText, correct, questionNumber);
         //result shouldn't return -1 
         assertEquals("Answer was submitted successfully.", expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         //fail("The test case is a prototype.");
-    }    
+    }
 }
