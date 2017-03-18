e971de610701e888ca4291c6194bac18a20fbe35
AllBecomesGood <paschnell@dundee.ac.uk>
2017-03-18 16:43:34 +0000

    Refactored TakingQuizModel.java

--- src/java/Models/Result.java
+++ src/java/Models/Result.java
@@ -1,15 +1,10 @@
 package Models;
 
-import Beans.Question;
-import Beans.QuizDetails;
 import Util.DBConnection;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.time.LocalDate;
-import java.util.ArrayList;
 
 /**
  * Refactored 18/03 by Philipp.
--- src/java/Models/TakingQuizModel.java
+++ src/java/Models/TakingQuizModel.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Models;
 
 import Util.DBConnection;
@@ -10,26 +5,37 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.time.LocalDate;
 
 /**
- *
+ * Refactored 18/03 by Philipp
  * @author Krasi
  */
-public class TakingQuizModel {
-    
-    public int savingGivenAnswer(int QuestionID,int AnswerID)
+public class TakingQuizModel 
+{
+    /** A method to store the answers a student has given for a certain quiz, 
+     *  so that later they can be retrieved and reviewed by the student.
+     * 
+     * @param QuestionID
+     * @param AnswerID
+     * @return 0 or row count of affected rows
+     */
+    public int saveGivenAnswer(int QuestionID, int AnswerID)
     {
         Connection con;
-        PreparedStatement st = null;
-        try{
+        PreparedStatement prepStatement = null;
+        int result;
+                
+        try
+        {
             con = DBConnection.createConnection();
-            st= con.prepareStatement("INSERT INTO given_answers (completed_questionID,given_answer) values(?,?)");
-            st.setInt(1, QuestionID);
-            st.setInt(2, AnswerID);
-            int result = st.executeUpdate();
-            st.clearParameters();
+            
+            prepStatement = con.prepareStatement("INSERT INTO given_answers (completed_questionID,given_answer) VALUES(?,?)");
+            prepStatement.setInt(1, QuestionID);
+            prepStatement.setInt(2, AnswerID);
+            result = prepStatement.executeUpdate(); // Returns 0 for failure or row count (rows that were affected) if it worked.
+            prepStatement.clearParameters();
+            
             con.close();
             return result;
         }
@@ -37,55 +43,66 @@ public class TakingQuizModel {
         {
             e.printStackTrace();
         }
-        return 0;
+        return 0; // DB-connection problem or update failure.
     }
     
+    /** This method saves details of a by a student completed quiz to the DB. E.g. number of Attempts, Score and more.
+     * 
+     * @param userID
+     * @param quizID
+     * @param score
+     * @param date
+     * @return 0 or row count of affected rows
+     */
     public int saveCompletedQuiz(int userID,int quizID,int score, LocalDate date)
     {
         Connection con;
-        PreparedStatement st = null;
+        PreparedStatement prepStatement = null;
         PreparedStatement check = null;
-        try{
+        try
+        {
             con = DBConnection.createConnection();
             int result = 0;
-            int id=0;
-            check= con.prepareStatement("Select ID from completed_quiz where userID=? and quizID=?");
+            int id = 0;
+            check = con.prepareStatement("SELECT ID FROM completed_quiz WHERE userID=? AND quizID=?");
             check.setInt(1, userID);
             check.setInt(2, quizID);
+            
             ResultSet ids = check.executeQuery();
             while(ids.next())
             {
-                id=ids.getInt("ID");
+                id = ids.getInt("ID");
             }
-            System.out.println("The ID of a completed quiz:"+id);
-            if(id==0)
+            if(id == 0)
             {
-            st= con.prepareStatement("INSERT INTO completed_quiz (Attempts,userID,quizID,Score,date) values(1,?,?,?,?)");
-            st.setInt(1, userID);
-            st.setInt(2, quizID);
-            st.setInt(3, score);
-            st.setDate(4, java.sql.Date.valueOf(date));
-            result = st.executeUpdate();
+                prepStatement= con.prepareStatement("INSERT INTO completed_quiz (Attempts,userID,quizID,Score,date) VALUES(1,?,?,?,?)");
+                prepStatement.setInt(1, userID);
+                prepStatement.setInt(2, quizID);
+                prepStatement.setInt(3, score);
+                prepStatement.setDate(4, java.sql.Date.valueOf(date));
+                result = prepStatement.executeUpdate(); // Returns 0 for failure or row count (rows that were affected) if it worked.
             }
             else
             {
-            int attempt = 0;
-            check.clearParameters();
-            check= con.prepareStatement("Select Attempts from completed_quiz where ID=?");
-            check.setInt(1, id);
-            ResultSet attempts = check.executeQuery();
-            while(attempts.next())
-            {
-                attempt=attempts.getInt("Attempts");
-            }    
-            st= con.prepareStatement("Update completed_quiz Set Attempts=? and Score=? and date=? where ID=?");
-            st.setInt(1, (attempt+1));
-            st.setInt(2, score);
-            st.setDate(3, java.sql.Date.valueOf(date));
-            st.setInt(4, id);
-            result = st.executeUpdate(); 
+                int attempt = 0;
+                check.clearParameters();
+                check = con.prepareStatement("SELECT Attempts FROM completed_quiz WHERE ID=?");
+                check.setInt(1, id);
+                
+                ResultSet attempts = check.executeQuery();
+                while(attempts.next())
+                {
+                    attempt = attempts.getInt("Attempts");
+                }    
+                prepStatement = con.prepareStatement("UPDATE completed_quiz SET Attempts=? AND Score=? AND date=? WHERE ID=?");
+                prepStatement.setInt(1, (attempt+1));
+                prepStatement.setInt(2, score);
+                prepStatement.setDate(3, java.sql.Date.valueOf(date));
+                prepStatement.setInt(4, id);
+                result = prepStatement.executeUpdate(); // Returns 0 for failure or row count (rows that were affected) if it worked.
             }
-            st.clearParameters();
+            prepStatement.clearParameters();
+            
             con.close();
             return result;
         }
@@ -93,7 +110,7 @@ public class TakingQuizModel {
         {
             e.printStackTrace();
         }
-        return 0;
+        return 0; // DB-connection problem or update failure.
     }
     
 }
